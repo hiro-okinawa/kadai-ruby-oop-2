5492a1b2a2a2fa200432370b5a60d5db7e98f311
hiro.oshiro <hiro.oshiro123gmail.com>
2021-08-22 13:58:01 +0000

    update-mentors

--- mentor.rb
+++ mentor.rb
@@ -1,4 +1,4 @@
-class Mentor
+class Mentor#スーパークラスの定義
   attr_accessor :name
   def initialize(name)
     self.name = name
@@ -6,14 +6,18 @@ class Mentor
   def job
     puts " #{self.name}です。私は現役のITプロフェッショナルです。"
   end
-end
+end#ここまでスーパークラス
 
-class RailsMentor < Mentor
+class RailsMentor < Mentor#サブクラスの定義
   def job
     puts " #{self.name}です。私はRubyとRailsでWebアプリケーションを作ります。"
   end
-end
+end#ここまでサブクラス
+
+#インスタンスの生成
 kirameki = Mentor.new("煌木")
 akaide = RailsMentor.new("赤出")
+
+#メソッドの呼び出し
 kirameki.job
 akaide.job
